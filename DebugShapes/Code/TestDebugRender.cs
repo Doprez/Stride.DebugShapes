b1aa1848ed5360d2221e4329b4996b0717678f7a
Doprez <73259914+Doprez@users.noreply.github.com>
2023-12-12 12:05:32 -0700

    initial commit

---
+++ DebugShapes/Code/TestDebugRender.cs
@@ -0,0 +1,23 @@
+﻿using Stride.Core.Mathematics;
+using Stride.Engine;
+using Stride.Rendering.Compositing;
+
+namespace DebugShapes.Code;
+public class TestDebugRender : SyncScript
+{
+
+	private ImmediateDebugRenderSystem debugRender;
+
+	public override void Start()
+	{
+		base.Start();
+
+		debugRender = Services.GetService<ImmediateDebugRenderSystem>();
+		debugRender.Visible = true;
+	}
+
+	public override void Update()
+	{
+		debugRender.DrawArrow(Vector3.Zero, Vector3.UnitX, color: Color.Red, depthTest: false);
+	}
+}
